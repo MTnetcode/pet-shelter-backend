ec540eefbfbf86ee945a5ff0ec02c4582e302d80
Tomas Storc <Ttomas.storc@gmail.com>
2021-05-28 11:10:22 +0200

    connect to mongo

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`connection to mongo ok`);
+  })
+  .catch((err) => {
+    console.log(`connection to mongo failed error: ${err}`);
+  });
+
 const petsApiRoute = require("./routes/api/pets.js");
 app.use("/api/pets", petsApiRoute);
 
