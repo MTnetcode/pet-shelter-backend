647982982b257938f8880ee9fe0e02f9e3f235e3
Tomas Storc <tomas.storc@gmail.com>
2021-05-28 21:53:06 +0200

    search pets by category

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Pets = require("../../models/Pets.js");
 
-router.get("/pets", (req, res) => {
-  res.json({ data: "test" });
+router.get("/", (req, res) => {
+  Pets.find({}, (err, foundPets) => {
+    if (err) {
+      res.json({ err });
+    }
+    if (!foundPets) {
+      res.json({ foundPets: "no pet found" });
+    } else {
+      res.json({ foundPets });
+    }
+  });
+});
+
+router.get("/:category", (req, res) => {
+  const { category } = req.params;
+  console.log(category);
+  if (category !== "dogs" && category !== "cats") {
+    res.json({ err: "category which you have specified is not valid" });
+  } else {
+    Pets.find({ category }, (err, foundPets) => {
+      if (err) {
+        res.json({ err });
+      }
+      if (!foundPets) {
+        res.json({ foundPets: "no pet found" });
+      } else {
+        res.json({ foundPets });
+      }
+    });
+  }
 });
 
 module.exports = router;
