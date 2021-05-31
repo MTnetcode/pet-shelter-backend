deb0f1a2e53ee56e49e56b3b66340b82b89f3cdf
Tomas Storc <tomas.storc@gmail.com>
2021-05-31 18:07:29 +0200

    added register new user

--- models/Users.js
+++ models/Users.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema({
   username: String,
   password: String,
+  role: String,
 });
 
 const User = mongoose.model("User", usersSchema);
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,22 +11,59 @@ router.post("/login", (req, res) => {
       res.json({ err });
     } else {
       if (!foundUser) {
-        res.json({ msg: "No user with specified username" });
+        res.json({ err: "No user with specified username" });
       } else {
         bcrypt.compare(password, foundUser.password, (err, isMatch) => {
           if (err) {
             res.json({ err });
           } else {
             if (!isMatch) {
-              res, json({ msg: "incorrect password, please try again" });
+              res, json({ err: "incorrect password, please try again" });
             } else {
               const payload = {
                 _id: foundUser._id,
                 username: foundUser.username,
+                role: foundUser.role,
               };
               const token = jwt.sign(payload, process.env.JWT_SECRET);
               res.json({ token });
             }
+          }
+        });
+      }
+    }
+  });
+});
+
+router.post("/register", (req, res) => {
+  const { username, password, role } = req.body;
+  User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      res.json({ err });
+    } else {
+      if (foundUser) {
+        res.json({ err: "User with given username already exists" });
+      } else {
+        bcrypt.hash(password, 10, (err, hashedPassword) => {
+          if (err) {
+            res.json({ err });
+          } else {
+            const newUser = new User({
+              username,
+              password,
+              role,
+            });
+            newUser
+              .save()
+              .then((user) => {
+                res.json({
+                  success: "new user successfully created",
+                  user,
+                });
+              })
+              .catch((err) => {
+                res.json({ err });
+              });
           }
         });
       }
