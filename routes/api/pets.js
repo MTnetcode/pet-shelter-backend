dfa761632bcde663ffe7c504b1209b752d89b720
Tomas Storc <tomas.storc@gmail.com>
2021-06-01 07:47:43 +0200

    post only with valid login

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const jwt = require("jsonwebtoken");
 const Pets = require("../../models/Pets.js");
+
+const protectPostRoute = (req, res, next) => {
+  if (!req.headers.authorization) {
+    res.json({ err: "no authorization header was send" });
+  } else {
+    const token = req.headers.authorization.split(" ")[1];
+    if (token === null) {
+      res.json({ err: "authorization header was sent but no token provided" });
+    } else {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          res.json({ err: "invalid token" });
+        } else {
+          if (!decoded) {
+            res.json({ err: "token could not be decoded" });
+          } else {
+            next();
+          }
+        }
+      });
+    }
+  }
+};
+
 let upload = multer({
   dest: "public/images",
 });
@@ -37,7 +62,7 @@ router.get("/:category", (req, res) => {
   }
 });
 
-router.post("/", upload.single("img"), (req, res) => {
+router.post("/", protectPostRoute, upload.single("img"), (req, res) => {
   const { name, text, category } = req.body;
   const img = `http://petshelter-api.mtnetcode.com/images/${req.file.filename}`;
   const newPet = new Pets({ name, text, img, category });
